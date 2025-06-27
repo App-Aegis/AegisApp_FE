a42817dad300b090272d4efdc833c8e7cef0098b
xhuyz <huylovengoc35@gmail.com>
2025-06-27 16:42:52 +0700

    prepare next build

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,5 @@
-import { useEffect } from 'react';
-import { useRouter } from 'expo-router';
+import { Redirect } from 'expo-router';
 
 export default function Index() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      router.replace('/splash'); // an toàn hơn push
-    }, 0); // Hoặc thêm độ trễ nhỏ nếu muốn
-    return () => clearTimeout(timeout);
-  }, []);
-
-  return null;
+  return <Redirect href="/splash" />;
 }
-
